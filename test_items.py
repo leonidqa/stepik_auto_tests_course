9d595e1bc10e0303a9aff23246e2c5b150ce5d7a
Leonid <lbobrovskiy21@gmail.com>
2024-10-31 15:43:20 +0300

    добавил test_items

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+def test_add_to_cart_button_is_displayed(browser):
+    browser.get(link)
+    time.sleep(10)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button, 'Button not found!'
