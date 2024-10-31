b813477a149ea9b07159daceedf2c74085fa1ce0
Leonid <lbobrovskiy21@gmail.com>
2024-10-31 15:46:01 +0300

    убрал таймаут в test_items

--- test_items.py
+++ test_items.py
@@ -8,6 +8,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
-    time.sleep(10)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button, 'Button not found!'
